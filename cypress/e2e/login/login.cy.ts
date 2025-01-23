4e10940402999c739f858a0e0c79dd61de4710c1
eloi-taniguti <eloi.taniguti@gmail.com>
2025-01-23 14:01:39 -0300

    chore: move users credendial to fixture

--- cypress/e2e/login/login.cy.ts
+++ cypress/e2e/login/login.cy.ts
@@ -1,21 +1,7 @@
 /// <reference types="cypress" />
 
 import LoginPage from '../../pages/Login'
-
-const invalid_credentials = [
-  {
-      'username': 'Demouser',
-      'password': 'abc123'
-  },
-  {
-      'username': 'demouser_',
-      'password': 'xyz'
-  },
-  {
-      'username': 'demouser',
-      'password': 'nanana'
-  }
-]
+import * as users from '../../fixtures/users.json'
 
 describe('Login page tests', () => {
   let login : LoginPage
@@ -25,11 +11,11 @@ describe('Login page tests', () => {
   })
 
   it('TC001_successful_login', () => {
-    login.loginAs('demouser', 'abc123')
+    login.loginAs(users.validUser.username, users.validUser.password)
     login.assertPageTitle('Invoice List')
   })
 
-  invalid_credentials.forEach((user) => {
+  users.invalidUsers.forEach((user) => {
     it(`TC002_invalid_login - ${user.username}`, () => {
       login.loginAs(user.username, user.password)
       login.assertErrorMessage('Wrong username or password.')  
